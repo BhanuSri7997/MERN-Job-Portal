dfd325bdc6992794583a988dcad986a8b3045ccf
BhanuSri7997 <23p31a1269@acet.ac.in>
2026-06-27 15:26:16 +0530

    Initial commit - MERN Job Portal

---
+++ frontend/src/redux/jobSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const jobSlice = createSlice({
+    name:"job",
+    initialState:{
+        allJobs:[],
+        allAdminJobs:[],
+        singleJob:null,
+        searchJobByText:"",
+        allAppliedJobs:[],
+        searchedQuery:"",
+    },
+    reducers:{
+        setAllJobs:(state, action) => {
+            state.allJobs = action.payload;
+        },
+        setSingleJob:(state,action) => {
+            state.singleJob = action.payload;
+        },
+        setAllAdminJobs:(state,action) => {
+            state.allAdminJobs = action.payload;
+        },
+        setSearchCompanyByText:(state,action) => {
+            state.searchJobByText = action.payload;
+        },
+        setAllAppliedJobs:(state, action) => {
+            state.allAppliedJobs = action.payload;
+        },
+        setSearchedQuery:(state,action) => {
+            state.searchedQuery = action.payload;
+        }
+    }
+});
+export const {
+    setAllJobs,
+    setSingleJob,
+    setAllAdminJobs,
+    setSearchCompanyByText,
+    setAllAppliedJobs,
+    setSearchedQuery} = jobSlice.actions;
+export default jobSlice.reducer;
