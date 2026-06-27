0d840e9aef36b32d51f0fe81336ac0469fd0404b
BhanuSri7997 <23p31a1269@acet.ac.in>
2026-06-27 16:30:04 +0530

    Update CORS for Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -24,9 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: "https://mern-job-portal-green.vercel.app",
+    credentials: true
+};
 
 app.use(cors(corsOptions));
 
